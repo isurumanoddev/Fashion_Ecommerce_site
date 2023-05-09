b950ed11ac86609675ddcd9b679f628681e31978
isurumanod <isurumanod99@gmail.com>
2023-05-09 14:43:54 +0530

    update

---
+++ store/urls.py
@@ -0,0 +1,11 @@
+from store import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('cart/', views.cart, name="cart"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
+    path('store/', views.store, name="store"),
+
+]
